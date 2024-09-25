a8499ffda39bc0b20914a5e04d220f5c0116292d
Brian Keller <161377758+BrianGKelelr@users.noreply.github.com>
2024-09-25 09:36:27 -0600

    Delete snakeTest/AITest.cpp

--- snakeTest/AITest.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <iostream>
-using namespace std;
-using namespace cv;
-
-void main() {	//we don't want to return anything here
-}
